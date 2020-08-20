4789d9e68990f0e62e3a3afc0a7d952c9275826e
Bosko Milekic <bosko.milekic@gmail.com>
2020-08-20 12:24:59 -0400

    Get rid of Pods and demo-ios-swift.xcworkspace as these are build artifacts

--- Pods/Target Support Files/OptableSDK/OptableSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OptableSDK : NSObject
-@end
-@implementation PodsDummy_OptableSDK
-@end
--- Pods/Target Support Files/Pods-demo-ios-swift-demo-ios-swiftUITests/Pods-demo-ios-swift-demo-ios-swiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_demo_ios_swift_demo_ios_swiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_demo_ios_swift_demo_ios_swiftUITests
-@end
