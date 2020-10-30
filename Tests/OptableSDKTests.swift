127573e3e856bd9d416c5a1429ded8a84cbb7a6e
Bosko Milekic <bosko.milekic@gmail.com>
2020-10-29 21:18:59 -0400

    Passing tests for OptableSDK.{eid,aaid,cid}

--- Tests/OptableSDKTests.swift
+++ Tests/OptableSDKTests.swift
@@ -10,18 +10,58 @@ import XCTest
 @testable import OptableSDK
 
 class OptableSDKTests: XCTestCase {
+    var sdk:OptableSDK!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sdk = OptableSDK.init(host: "127.0.0.1", app: "tests", insecure: true)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_eid_isCorrect() throws {
+        let expected = "e:a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"
+        XCTAssertEqual(expected, sdk.eid("123"))
+        XCTAssertEqual(expected, sdk.eid(" 123"))
+        XCTAssertEqual(expected, sdk.eid("123 "))
+        XCTAssertEqual(expected, sdk.eid(" 123 "))
+    }
+
+    func test_eid_ignoresCase() throws {
+        let var1 = "tEsT@FooBarBaz.CoM"
+        let var2 = "test@foobarbaz.com"
+        let var3 = "TEST@FOOBARBAZ.COM"
+        let var4 = "TeSt@fOObARbAZ.cOm"
+        let eid = sdk.eid(var1)
+
+        XCTAssertEqual(eid, sdk.eid(var2))
+        XCTAssertEqual(eid, sdk.eid(var3))
+        XCTAssertEqual(eid, sdk.eid(var4))
+    }
+
+    func test_aaid_isCorrectAndIgnoresCase() throws {
+        let expected = "a:ea7583cd-a667-48bc-b806-42ecb2b48606"
+
+        XCTAssertEqual(expected, sdk.aaid("ea7583cd-a667-48bc-b806-42ecb2b48606"))
+        XCTAssertEqual(expected, sdk.aaid("  ea7583cd-a667-48bc-b806-42ecb2b48606"))
+        XCTAssertEqual(expected, sdk.aaid("ea7583cd-a667-48bc-b806-42ecb2b48606  "))
+        XCTAssertEqual(expected, sdk.aaid("  ea7583cd-a667-48bc-b806-42ecb2b48606  "))
+        XCTAssertEqual(expected, sdk.aaid("EA7583CD-A667-48BC-B806-42ECB2B48606"))
+    }
+
+    func test_cid_isCorrect() throws {
+        let expected = "c:FooBarBAZ-01234#98765.!!!"
+
+        XCTAssertEqual(expected, sdk.cid("FooBarBAZ-01234#98765.!!!"))
+        XCTAssertEqual(expected, sdk.cid(" FooBarBAZ-01234#98765.!!!"))
+        XCTAssertEqual(expected, sdk.cid("FooBarBAZ-01234#98765.!!!  "))
+        XCTAssertEqual(expected, sdk.cid("  FooBarBAZ-01234#98765.!!!  "))
+    }
+
+    func test_cid_isCaseSensitive() throws {
+        let unexpected = "c:FooBarBAZ-01234#98765.!!!"
+
+        XCTAssertNotEqual(unexpected, sdk.cid("foobarBAZ-01234#98765.!!!"))
     }
 
     func testPerformanceExample() throws {
