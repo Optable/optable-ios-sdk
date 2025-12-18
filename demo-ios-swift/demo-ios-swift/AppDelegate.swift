1223619e3da1e095c947f5467ea67b5a83405a53
vladislav-yermakov <yermakov.vladslav1@gmail.com>
2025-12-18 13:03:07 +0100

    - Updated demo-ios-swift

--- demo-ios-swift/demo-ios-swift/AppDelegate.swift
+++ demo-ios-swift/demo-ios-swift/AppDelegate.swift
@@ -6,8 +6,8 @@
 //  See LICENSE for details.
 //
 
-import UIKit
 import OptableSDK
+import UIKit
 
 // The OPTABLE global points to an instance of OptableSDK which is initialized in the AppDelegate application() method at app launch.
 // While we could have initialized the global directly here, due to Swift lazy-loading this would delay initialization to the first
@@ -16,14 +16,19 @@ import OptableSDK
 // ideally have init happen well before the first usage/API call if possible.
 var OPTABLE: OptableSDK?
 
+// MARK: - AppDelegate
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        // See comment further above on why we are initializing OptableSDK() from here:
-        OPTABLE = OptableSDK(host: "sandbox.optable.co", app: "ios-sdk-demo")
+
+        let config = OptableConfig(
+            tenant: "prebidtest",
+            originSlug: "ios-sdk",
+            host: "prebidtest.cloud.optable.co",
+            skipAdvertisingIdDetection: false
+        )
+        OPTABLE = OptableSDK(config: config)
 
         return true
     }
--- demo-ios-swift/demo-ios-swift/IdentifyViewController.swift
+++ demo-ios-swift/demo-ios-swift/IdentifyViewController.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
+import OptableSDK
 
 class IdentifyViewController: UIViewController {
-
     // MARK: Properties
-    @IBOutlet weak var identifyInput: UITextField!
-    @IBOutlet weak var identifyButton: UIButton!
-    @IBOutlet weak var identifyIDFA: UISwitch!
-    @IBOutlet weak var identifyOutput: UITextView!
+    @IBOutlet var identifyInput: UITextField!
+    @IBOutlet var identifyButton: UIButton!
+    @IBOutlet var identifyIDFA: UISwitch!
+    @IBOutlet var identifyOutput: UITextView!
 
     // MARK: Actions
-    
+
     // dispatchIdentify() is the action invoked on a click on the "Identify" UIButton in our demo app. It initiates
     // a call to the OptableSDK.identify() API and prints debugging information to the UI and debug console.
     @IBAction func dispatchIdentify(_ sender: UIButton) {
@@ -26,26 +26,35 @@ class IdentifyViewController: UIViewController {
             let aaid = identifyIDFA.isOn as Bool
 
             identifyOutput.text = "Calling /identify API with:\n\n"
-            if (email != "") {
+            if email != "" {
                 identifyOutput.text += "Email: " + email + "\n"
             }
             identifyOutput.text += "IDFA: " + String(aaid) + "\n"
 
-            try OPTABLE!.identify(email: email, aaid: aaid) { result in
-                switch result {
-                case .success(let response):
-                    print("[OptableSDK] Success on /identify API call: response.statusCode = \(response.statusCode)")
-                    DispatchQueue.main.async {
-                        self.identifyOutput.text += "\n✅ Success."
-                    }
+            let idfa = "06DE8C6A-A431-4235-A262-E3A9C2CCEB34"
+            let gaid = "D04BB8C3-5A3E-4964-9757-D38365F59E6A"
+            let phoneNumber = "+1234567890"
+            let custom = "new-custom.ABC"
+            let custom9 = "custom-9-id"
 
-                case .failure(let error):
-                    print("[OptableSDK] Error on /identify API call: \(error)")
-                    DispatchQueue.main.async {
-                        self.identifyOutput.text += "\n🚫 Error: \(error)"
+            try OPTABLE!.identify(
+                OptableIdentifiers(emailAddress: email, phoneNumber: phoneNumber, appleIDFA: idfa, googleGAID: gaid, custom: ["c": custom, "c9": custom9]),
+                { result in
+                    switch result {
+                    case let .success(response):
+                        print("[OptableSDK] Success on /identify API call: response.statusCode = \(response.statusCode)")
+                        DispatchQueue.main.async {
+                            self.identifyOutput.text += "\n✅ Success. Response: \(response)"
+                        }
+
+                    case let .failure(error):
+                        print("[OptableSDK] Error on /identify API call: \(error)")
+                        DispatchQueue.main.async {
+                            self.identifyOutput.text += "\n🚫 Error: \(error)"
+                        }
                     }
                 }
-            }
+            )
 
         } catch {
             print("[OptableSDK] Exception: \(error)")
