c09c76894693f281e64c6b56c5883933fff51d7c
Olena Stepaniuk <92721590+OlenaPostindustria@users.noreply.github.com>
2026-04-27 11:45:59 +0300

    Fix SPM integration (#56)

--- Source/OptableSDK.swift
+++ Source/OptableSDK.swift
@@ -111,25 +111,6 @@ public extension OptableSDK {
             }
         })
     }
-
-    // MARK: Objective-C support
-    /**
-     This is the Objective-C compatible version of the `identify(ids, completion)` API.
-
-     Instead of completion callbacks, delegate methods are called.
-     */
-    @objc
-    func identify(_ ids: [OptableSDKIdentifier]) throws {
-        let bridgedIds = ids.compactMap({ OptableIdentifier(objc: $0) })
-        try self._identify(bridgedIds) { result in
-            switch result {
-            case let .success(response):
-                self.delegate?.identifyOk(response)
-            case let .failure(error as NSError):
-                self.delegate?.identifyErr(error)
-            }
-        }
-    }
 }
 
 // MARK: - Targeting
@@ -174,25 +155,6 @@ public extension OptableSDK {
                 try self._targeting(ids: ids, completion: { continuation.resume(with: $0) })
             } catch {
                 continuation.resume(throwing: error)
-            }
-        })
-    }
-
-    // MARK: Objective-C support
-    /**
-     This is the Objective-C compatible version of the `targeting(completion)` API.
-
-     Instead of completion callbacks, delegate methods are called.
-     */
-    @objc
-    func targeting(_ ids: [OptableSDKIdentifier]) throws {
-        let bridgedIds = ids.compactMap({ OptableIdentifier(objc: $0) })
-        try self._targeting(ids: bridgedIds, completion: { result in
-            switch result {
-            case let .success(optableTargeting):
-                self.delegate?.targetingOk(optableTargeting)
-            case let .failure(error as NSError):
-                self.delegate?.targetingErr(error)
             }
         })
     }
